75614cf16d2b2182426966554d4eb633be8182c5
Vivek <vivekkamble762033@gmail.com>
2026-04-22 19:27:59 +0530

    save changes

--- 22.Lists/Lists.md
+++ 22.Lists/Lists.md
@@ -3,3 +3,9 @@
 It is an data type in python.
 It is an ordered collection of data values in one single variable.
 Created by using [].
+
+- Allows duplicate elements
+- Mutable: list elements can be changed, updated, added, or removed after the list is created.
+- Ordered: elements maintain the order in which they are inserted.
+- Index-based: elements are accessed using their position, starting from index 0.
+- Heterogeneous: a list can store different data types such as integers, strings, booleans and even other lists.
--- 22.Lists/list.py
+++ 22.Lists/list.py
@@ -1,21 +1,43 @@
-# l = [3,5,6, 8, 9, 11, "Harry", True]
-# print(l)
-# print(type(l))
-# print(l[4])
-# print(l[0:4])
-# print(l[-3])
-# print(len(l)-3)
-# print(8 - 3)
-# print(5)
-# print(l[1:8:2])
-# if 7 in l:
-#     print("Yes")
-# else:
-#     print("NO")
 
-# fruits = ["mangos", "apple", "watermelon", "jackfruit","kiwi", "grapes", "orange", "dragonfruit", "banana"]
 
-# print(fruits[0:9:3])
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-LS = [ i for i in range(2,22,2)]
-print(LS)
+print(lst)
+
+fruits = ['Mango', 'Apple', 'Watermelon', 'Grapes', 'Banana', 'Kiwi']
+print(fruits)
+
+vue = ['Vivek', 2, 'James', (1, 2, 3), {'vivek':1, 'sujal':2}]
+print(vue)
+
+# . Using list() Constructor
+
+a = list((1, 2, 3, 'vivek', 'sujal'))
+print(a)
+
+ms = {'mango': 23, 'grapes':12}
+a = list(ms)
+print(a)
+
+#  Creating List with Repeated Elements
+
+a = [2] * 5
+b = [3] * 8
+
+print(a, b)
+
+
+'''
+Accessing List Elements
+Elements in a list are accessed using indexing. Python uses zero-based indexing, meaning a[0] represents the first element.
+Negative indexing is also supported, where -1 accesses the last element.'''
+
+
+a = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+print(len(a))
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+print(a[5])
