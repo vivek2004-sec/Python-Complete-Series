1864671842597552432327b43f5d48f3625cae05
Vivek <vivekkamble762033@gmail.com>
2026-05-04 23:52:22 +0530

    Iteration over dictionary

---
+++ 32.Dictionaries_methods/dict.py
@@ -0,0 +1,13 @@
+d = {1: 'Geeks', 2: 'For', 'age':22}
+
+# Iterate over keys
+for keys in d:
+    print(keys)
+
+#Iterate over values  
+for value in d.values():
+    print(value)
+    
+# Iterate over key-value pairs
+for key, value in d.items():
+    print(f"{keys}:{value}")
