513f66888038dcbc68c3b6a4c11f33f19380c514
Vivek <vivekkamble762033@gmail.com>
2026-05-04 20:06:22 +0530

    Added new dictionary changes

---
+++ 31.Dictonaries/dict.py
@@ -0,0 +1,52 @@
+vue = {
+    "name": "Vivek",
+    "middle-name": "Ranjit",
+    "last-name": "Kamble",
+    "city": "kagal",
+    "department": "CSE",
+    "year-of-study": "3rd"
+    
+}
+print(vue)
+
+# Access of values based on keys:
+print(vue["name"])
+print(vue["middle-name"])
+print(vue["last-name"])
+print(vue["city"])
+print(vue["department"])
+
+print(vue.get("name"))
+print(vue.get("last-name"))
+
+
+# Adding new Items in dictionary:
+ls = {1:"apple", 2:"mango", 3:"orange"}
+
+ls[4] = "Grapes"
+print(ls)
+
+# Updating an existing value:
+
+ls[1] = "watermelon"
+print(ls)
+
+# Removing Dictionary Items:
+
+ls = {1:"apple", 2:"mango", 3:"orange", 4: "grapes", 5: "kiwi", 6:"banana"}
+del ls[1]
+print(ls)
+# del: removes an item using its key
+
+print(ls.pop(2))
+# pop(): removes the item with the given key and returns its value
+
+d = {"name": "Vivek", "age": 21, "city": "Kolhapur"}
+
+result = d.popitem()
+
+print(result)  # ('city', 'Kolhapur')
+print(d)       # {'name': 'Vivek', 'age': 21}
+# It removes and returns the last inserted key-value pair as a tuple
+
+print(d.clear())
