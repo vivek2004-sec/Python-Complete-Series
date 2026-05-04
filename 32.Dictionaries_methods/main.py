948c36cc667065276287226801785002ef0afb0e
Vivek <vivekkamble762033@gmail.com>
2026-05-04 23:45:01 +0530

    Add new Dictionary methods

--- 32.Dictionaries_methods/main.py
+++ 32.Dictionaries_methods/main.py
@@ -1,7 +1,39 @@
-ep1 = {122: 45, 123: 89, 567: 69, 786: 90}
-ep2 = {22: 33, 222: 87, 87: 23}
-# ep1.pop(122)
-ep1.popitem()
-# del ep1
-del ep1[122]
-print(ep1)
+vue = {
+    "name": "Vivek",
+    "middle-name": "Ranjit",
+    "last-name": "Kamble",
+    "city": "kagal",
+    "department": "CSE",
+    "year-of-study": "3rd", 
+    "subject": ['maths', 'physics', 'computer networks', 'csi'],
+    "marks": (90, 89, 88),
+    "group": {"amey", "vivek"},
+    "info": {"vivek":"captain", "amey": "vice-captain"}
+    
+    
+    
+}
+
+print(vue.keys())
+print(vue.values())
+print(vue.items())
+print(vue.get("subject"))
+vue.update({"last-name": "kagale", "department": "it", "standard": "undergraduate"})
+print(vue)
+print(vue.pop("city"))
+
+
+d  = {"name": "Vivek", "age": 21}
+d2 = d.copy()
+
+d2["name"] = "Kamble"   # changes only d2
+
+print(d)   # {'name': 'Vivek', 'age': 21}   ← original unchanged
+print(d2)  # {'name': 'Kamble', 'age': 21}
+
+d = {"name": "", "age": 21}
+
+print(d.setdefault("name", "Unknown"))   # Vivek   ← key exists
+print(d.setdefault("city", "Kolhapur"))  # Kolhapur ← key added
+
+print(d)  # {'name': 'Vivek', 'age': 21, 'city': 'Kolhapur'}
