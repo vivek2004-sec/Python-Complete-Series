a1d8b2be695a18f2b5d44f4945bc3021ab6e6f15
Vivek <vivekkamble762033@gmail.com>
2026-05-23 21:30:25 +0530

    added modified version of code

--- 46.File_io_methods/main.py
+++ 46.File_io_methods/main.py
@@ -6,12 +6,20 @@
 #     print(line.strip())
 # f.close()
 
-
+i = 0
 with open("46.File_io_methods/myfile.txt", 'r') as f:
     while True:
+        i += 1
+
         line = f.readline()
         if not line:
             break
+        m1 = line.split(",")[0]
+        m2 = line.split(",")[0]
+        m3 = line.split(",")[0]
+        print(f"Marks of student {i} in maths : {m1} ")
+        print(f"Marks of student {i} in maths : {m2} ")
+        print(f"Marks of student {i} in maths : {m3} ")
         print(line.strip())
 
 
