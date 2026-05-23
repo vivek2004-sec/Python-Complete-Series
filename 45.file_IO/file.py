5caf9f58a114158e7d76b473d0bfd6fe6dbcf4be
Vivek <vivekkamble762033@gmail.com>
2026-05-23 20:56:56 +0530

    created new file

--- 45.file_IO/file.py
+++ 45.file_IO/file.py
@@ -12,15 +12,19 @@
 # print("Details saved!")
 
 
-name = input("Enter the name: ")
-age = int(input("Enter the age: "))
-city = input("Enter the city: ")
+# name = input("Enter the name: ")
+# age = int(input("Enter the age: "))
+# city = input("Enter the city: ")
 
 
-with open("45.file_IO/details.txt", "a") as f:
-    f.write(f"Name -> {name}\n")
-    f.write(f"Age -> {age}\n")
-    f.write(f"City -> {city}\n")
+# with open("45.file_IO/details.txt", "a") as f:
+#     f.write(f"Name -> {name}\n")
+#     f.write(f"Age -> {age}\n")
+#     f.write(f"City -> {city}\n")
     
     
-print("Details saved!")
+# print("Details saved!")
+
+
+import os
+os.mkdir("46.File_io_methods")
