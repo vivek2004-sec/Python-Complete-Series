6022693bc341acf1557e9d0ee7118afea63604d5
Vivek <vivekkamble762033@gmail.com>
2026-05-23 21:20:25 +0530

    created better version of the code

--- 46.File_io_methods/main.py
+++ 46.File_io_methods/main.py
@@ -1,6 +1,18 @@
-f = open("46.File_io_methods/myfile.txt", 'r')
-while True: 
-    line  = f.readline()
-    if not line:
-        break
-    print(line)
+# f = open("46.File_io_methods/myfile.txt", 'r')
+# while True: 
+#     line  = f.readline()
+#     if not line:
+#         break
+#     print(line.strip())
+# f.close()
+
+
+with open("46.File_io_methods/myfile.txt", 'r') as f:
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        print(line.strip())
+
+
+  
