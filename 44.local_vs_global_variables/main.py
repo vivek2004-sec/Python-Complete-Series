17550a32bd5937cd83dbd476681427f5181b8112
Vivek <vivekkamble762033@gmail.com>
2026-05-23 18:10:31 +0530

    add new examples

--- 44.local_vs_global_variables/main.py
+++ 44.local_vs_global_variables/main.py
@@ -1,11 +1,21 @@
-x = 4
-print(x)
 
-def hello():
-    x = 5
-    print(f"The local variable is {x} ")
-    print("Hello!")
-    
-print(f"The local variable is {x}")
-hello()
-print(f"The global variable is {x}")
+
+
+name = "Vivek"        # global variable
+
+def my_function():
+    name = "Kamble"   # local variable (different!)
+    print(name)       # prints local
+
+my_function()
+print(name)           # prints 
+
+
+# x = 5
+
+# def vue():
+#     x = 7
+#     print(x)
+
+# vue()
+# print(x)
