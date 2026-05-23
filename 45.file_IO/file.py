e568b0d2de5d217005e228a9fa12082bb0d070ed
Vivek <vivekkamble762033@gmail.com>
2026-05-23 20:37:58 +0530

    new program added

--- 45.file_IO/file.py
+++ 45.file_IO/file.py
@@ -1,9 +1,23 @@
+# name = input("Enter the name: ")
+# age = int(input("Enter the age: "))
+# city = input("Enter the city: ")
+
+
+# with open("45.file_IO/details.txt", "w") as f:
+#     f.write(f"Name -> {name}\n")
+#     f.write(f"Age -> {age}\n")
+#     f.write(f"City -> {city}\n")
+    
+    
+# print("Details saved!")
+
+
 name = input("Enter the name: ")
 age = int(input("Enter the age: "))
 city = input("Enter the city: ")
 
 
-with open("45.file_IO/details.txt", "w") as f:
+with open("45.file_IO/details.txt", "a") as f:
     f.write(f"Name -> {name}\n")
     f.write(f"Age -> {age}\n")
     f.write(f"City -> {city}\n")
