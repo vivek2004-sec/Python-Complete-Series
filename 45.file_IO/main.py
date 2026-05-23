858acfe9947db254168b6cab3b00e0a6cca03aee
Vivek <vivekkamble762033@gmail.com>
2026-05-23 19:01:52 +0530

    added new file

--- 45.file_IO/main.py
+++ 45.file_IO/main.py
@@ -1,3 +1,7 @@
-f = open("mylife.txt", 'w')
-f.write("Hello this is vivek.")
-f.close()
+# f = open("mylife.txt", 'w')
+# f.write("Hello this is vivek.")
+# f.close()
+
+m = open("45.file_IO/vivek.txt", "w")
+m.write("Hello world!")
+m.close()
