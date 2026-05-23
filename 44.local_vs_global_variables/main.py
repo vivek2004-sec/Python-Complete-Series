fe34b7140612619d994d6bad9d8fb09e48301171
Vivek <vivekkamble762033@gmail.com>
2026-05-23 18:42:53 +0530

    solved the question

--- 44.local_vs_global_variables/main.py
+++ 44.local_vs_global_variables/main.py
@@ -58,3 +58,38 @@ increment()
 increment()
 increment()
 print(count)
+
+
+# Question: 
+# Create a program that:
+# → Has a global score = 0
+# → Has a function add_score(points)
+#    that adds points to score
+# → Has a function reset_score()
+#    that resets score to 0
+# → Print score after every call
+
+# Expected Output:
+# Score → 10
+# Score → 30
+# Score → 60
+# Score → 0   ← after reset
+
+
+score = 0
+
+def add_score(points):
+    global score
+    score += points
+    print(score)
+    
+def reset_score():
+    global score
+    score = 0 
+    print(score)
+
+
+add_score(10)
+add_score(20)
+add_score(30)
+reset_score()
