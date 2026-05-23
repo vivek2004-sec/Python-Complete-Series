6d419fd682089deed836ab1e49ef3b44c492b25e
Vivek <vivekkamble762033@gmail.com>
2026-05-23 18:18:23 +0530

    Added global variable usage and corrected local variable scope

--- 44.local_vs_global_variables/main.py
+++ 44.local_vs_global_variables/main.py
@@ -1,14 +1,14 @@
 
 
 
-name = "Vivek"        # global variable
+# name = "Vivek"        # global variable
 
-def my_function():
-    name = "Kamble"   # local variable (different!)
-    print(name)       # prints local
+# def my_function():
+#     name = "Kamble"   # local variable (different!)
+#     print(name)       # prints local
 
-my_function()
-print(name)           # prints 
+# my_function()
+# print(name)           # prints 
 
 
 # x = 5
@@ -21,13 +21,26 @@ print(name)           # prints
 # print(x)
 
 
-x = 7
+# x = 7
 
-def rishi():
-    y = 8
-    print(y)
+# def rishi():
+#     y = 8
+#     print(y)
 
 
-rishi()
-print(x)
-print(y)
+# rishi()
+# print(x)
+# print(y)  Throws an error as y is local variable and cannot be exist outside the function.
+
+
+# Gobal Keyword:
+
+name = "kamble"
+
+def my_name():
+    global name
+    name = "vivek"
+    print(name)
+
+my_name()
+print(name)
