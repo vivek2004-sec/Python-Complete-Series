875a5d1749002387ff8dd16ec48db8cd420405a4
Vivek <vivekkamble762033@gmail.com>
2026-05-23 19:43:01 +0530

    opened new files

--- 45.file_IO/main.py
+++ 45.file_IO/main.py
@@ -1,7 +1,11 @@
-f = open("45.file_IO/mylife.txt", 'w')
-f.write("Hello this is vivek.")
-f.close()
+# f = open("45.file_IO/mylife.txt", 'w')
+# f.write("Hello this is vivek.")
+# f.close()
 
 # m = open("45.file_IO/vivek.txt", "w")
 # m.write("Hello world!")
 # m.close()
+
+f = open("requirements.txt", "r")
+text = f.read()
+print(text)
