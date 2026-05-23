5d6fdd32d6bbd7df26a8568d47aecf7b55aa9a77
Vivek <vivekkamble762033@gmail.com>
2026-05-23 19:59:50 +0530

    add new file mode

--- 45.file_IO/main.py
+++ 45.file_IO/main.py
@@ -1,6 +1,6 @@
-f = open("45.file_IO/mylife.txt", 'a')
-f.write("Hello this is vivek.")
-f.close()
+# f = open("45.file_IO/mylife.txt", 'a')
+# f.write("Hello this is vivek.")
+# f.close()
 
 # m = open("45.file_IO/vivek.txt", "w")
 # m.write("Hello world!")
@@ -10,3 +10,9 @@ f.close()
 # text = f.read()
 # print(text)
 # f.close()
+
+# Read Line by Line: 
+f = open("45.file_IO/mylife.txt", "r")
+line = f.readline()
+print(line)
+f.close()
