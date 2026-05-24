5369aa6921cd481b2af76a7da3ab65fe927c4cea
Vivek <vivekkamble762033@gmail.com>
2026-05-24 19:23:59 +0530

    filter function

--- 47.lambda_function/main.py
+++ 47.lambda_function/main.py
@@ -31,3 +31,11 @@ numbers = [1, 2, 3, 4, 5]
 
 squared = list(map(lambda x: x ** 2, numbers))
 print(squared)          # [1, 4, 9, 16, 25]
+
+
+# filter() filters items based on condition
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+evens = list(filter(lambda x: x % 2 == 0, numbers))
+print(evens)            # [2, 4, 6, 8]
