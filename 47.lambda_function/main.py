d90af8ac5c32f44bbccd98c45f902c519e691c9c
Vivek <vivekkamble762033@gmail.com>
2026-05-24 19:21:14 +0530

    made some new changes

--- 47.lambda_function/main.py
+++ 47.lambda_function/main.py
@@ -11,3 +11,8 @@ print("Area of reactangle is:",aor(3,4))
 area_of_square = lambda s: s**2
 
 print("Area of square is: ", area_of_square(5))
+
+
+is_even = lambda x: x % 2 == 0
+print(is_even(4))       # True
+print(is_even(5))       # False
