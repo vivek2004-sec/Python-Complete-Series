8a6af8e5bcdf987bbd377affc4cf7eb6c0215215
Vivek <vivekkamble762033@gmail.com>
2026-05-24 19:21:54 +0530

    changes

--- 47.lambda_function/main.py
+++ 47.lambda_function/main.py
@@ -16,3 +16,7 @@ print("Area of square is: ", area_of_square(5))
 is_even = lambda x: x % 2 == 0
 print(is_even(4))       # True
 print(is_even(5))       # False
+
+
+maximum = lambda a, b: a if a > b else b
+print(maximum(10, 20))  # 20
