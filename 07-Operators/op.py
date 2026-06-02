62a30bd41bb103f2a95c652849ce49d9c6fcc168
Vivek <vivekkamble762033@gmail.com>
2026-06-02 21:06:16 +0530

    added new example

--- 07-Operators/op.py
+++ 07-Operators/op.py
@@ -2,3 +2,13 @@ a = 10000
 b = 10000
 print(a == b)   # True  → same value
 print(a is b)   # False → different objects (no caching above 256)
+
+
+a = [1,2,3]
+b = [1,2,3]
+print(a==b)
+print(a is b)
+'''
+Here a == b says that a and b both contains same values.
+and a is b says both are different lists located in memory.
+'''
