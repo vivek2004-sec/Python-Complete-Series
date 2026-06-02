3ebbe27f58f580e51f76b3f94dcaa8f4b1bddd0a
Vivek <vivekkamble762033@gmail.com>
2026-06-02 21:23:23 +0530

    addded new example of is not

--- 07-Operators/op.py
+++ 07-Operators/op.py
@@ -13,6 +13,6 @@ Here a == b says that a and b both contains same values.
 and a is b says both are different lists located in memory.
 '''
 a = [1,2,3]
-b = a
+b = [1,2,3]
 print(a is b)
-print(a == b)
+print(a is not b)
