bcfac38a5f9e550cf6e94b6ce4865d0ace7e3444
Vivek <vivekkamble762033@gmail.com>
2026-06-03 13:36:32 +0530

    added new text

--- 12-String-slicing-and-Operations/str.py
+++ 12-String-slicing-and-Operations/str.py
@@ -2,10 +2,11 @@ name="Vivek,Harry,Chetan,Atharv,Sanket"
 v = "Chetan"
 print(v in name)  # Membership Operator
 print(len(name))
-print(name[0:18])
-print(name[0:5])
-print(name[0:11])
-print(name[6:11])
+# print(name[0:18])
+# print(name[0:5])
+# print(name[0:11])
+# print(name[6:11])
+print(name[5])
 
 print(len(name))
 
