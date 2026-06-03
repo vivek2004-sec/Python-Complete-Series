d8891d744d9a31961db61ba7861272f4d8a7a6dc
Vivek <vivekkamble762033@gmail.com>
2026-06-03 18:23:44 +0530

    added new methods

--- 13-string-methods/main.py
+++ 13-string-methods/main.py
@@ -9,3 +9,17 @@ s.title()        # "Hello World"
 s.swapcase()     # "HELLO WORLD" → swaps upper/lower
 
 
+# Search & Check:
+
+s = "hello world"
+
+s.find("world")      # 6  → returns index, -1 if not found
+s.index("world")     # 6  → same but raises ValueError if not found
+s.count("l")         # 3
+s.startswith("hello") # True
+s.endswith("world")   # True
+"world" in s          # True
+
+name = 'vivek kamble'
+print(name.find('kamble'))
+print(name[6:12])
