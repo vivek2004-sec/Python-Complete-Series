50495ab7b73afca0109ea2b202b80e2d07fd843c
Vivek <vivekkamble762033@gmail.com>
2026-06-03 18:12:37 +0530

    added new things

--- 12-String-slicing-and-Operations/str.py
+++ 12-String-slicing-and-Operations/str.py
@@ -23,25 +23,25 @@ print(fruit[0:len(fruit)-3])
 print(fruit[-3:-1])
 print(fruit[-4:-2])
 
-# city= "Mumbai"
-# len1=len(city)
-# print("the length of city is", len1,"defined")
+city= "Mumbai"
+len1=len(city)
+print("the length of city is", len1,"defined")
 
-# print(city[0:1])
-# print(city[0:2])
-# print(city[0:3])
-# print(city[0:4])
-# print(city[0:5])
-# print(city[0:6])
-# print(city[:6])
-# print(city[0:-3])
+print(city[0:1])
+print(city[0:2])
+print(city[0:3])
+print(city[0:4])
+print(city[0:5])
+print(city[0:6])
+print(city[:6])
+print(city[0:-3])
 
-# vue= "Kamble"
-# print(vue[0:-2])
-# print(vue[0:-3])
-# print(vue[0:-1])
+vue= "Kamble"
+print(vue[0:-2])
+print(vue[0:-3])
+print(vue[0:-1])
 
 
-# nm = "Harry"
-# print(nm[-4:-2])
-# print(nm[-3:-1])
+nm = "Harry"
+print(nm[-4:-2])
+print(nm[-3:-1])
