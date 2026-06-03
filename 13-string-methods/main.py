efc03e07f7a93e46ea61e573ea0c2efc9b4e1b1c
Vivek <vivekkamble762033@gmail.com>
2026-06-03 18:16:01 +0530

    added new methods

---
+++ 13-string-methods/main.py
@@ -0,0 +1,9 @@
+# Case Methods:
+
+s = "hello world"
+
+s.upper()        # "HELLO WORLD"
+s.lower()        # "hello world"
+s.capitalize()   # "Hello world"
+s.title()        # "Hello World"
+s.swapcase()     # "HELLO WORLD" → swaps upper/lower
