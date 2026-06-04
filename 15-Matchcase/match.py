bbe37596a2ffa60dd157c7b8772e1e7c48997fcc
Vivek <vivekkamble762033@gmail.com>
2026-06-04 20:08:59 +0530

    added new things

--- 15-Matchcase/match.py
+++ 15-Matchcase/match.py
@@ -12,7 +12,7 @@ match x:
         
         print("x is zero.")
         # case with if - condition.
-    case 6 if x % 2 == 0:
+    case 4 if x % 2 == 0:
         print("x % 2 == 0 and case is 4")
     case _ if x < 10: 
         print("x is < 10.")
