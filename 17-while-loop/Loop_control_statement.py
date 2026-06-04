22afbd826e512699bb99cda13b7dabd54e341c2a
Vivek <vivekkamble762033@gmail.com>
2026-06-04 20:49:46 +0530

    added new things

--- 17-while-loop/Loop_control_statement.py
+++ 17-while-loop/Loop_control_statement.py
@@ -11,10 +11,10 @@ for i in range(1, 6):
    
 # 🔹 2. continue
 
-# for i in range(1,6):
-#     if i == 3:
-#         continue
-#     print(i)
+for i in range(1,6):
+    if i == 3:
+        continue
+    print(i)
     
     
 # 🔹 3. pass
