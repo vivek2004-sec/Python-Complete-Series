843871562eb65e8f0c8a13fdba7a1a0a99234e07
Vivek <vivekkamble762033@gmail.com>
2026-06-04 20:54:44 +0530

    added new things

--- 18.break_&_continue/break.py
+++ 18.break_&_continue/break.py
@@ -1,5 +1,6 @@
 # Break statement: It enables a program to skip over a part of the code.A break statement terminates the very loop it lies within.
 # exits loop.
+
 # for i in range(9):
 #     print("5 X", i+1, "=", 5 * (i+1))
 #     if (i == 5):
@@ -16,9 +17,9 @@
 # continue: exits the iteration
 # skips the rest of the code in the loop and moves to the next iteration.   
 
-for i in range(10):
-    if (i == 6):
-     print("Skip the iteration.")
-     continue
-    print("5 X", i, "=", 5 * (i))
-print("Done with the loop.")
+# for i in range(10):
+#     if (i == 6):
+#      print("Skip the iteration.")
+#      continue
+#     print("5 X", i, "=", 5 * (i))
+# print("Done with the loop.")
