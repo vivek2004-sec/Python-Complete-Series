2d8b729f9394d8537c847de359205b31bf45a40a
Vivek <vivekkamble762033@gmail.com>
2026-06-05 23:48:04 +0530

    added new things

--- 18.break_&_continue/break.py
+++ 18.break_&_continue/break.py
@@ -8,11 +8,11 @@ for i in range(10):
  
 print("Done with the loop.")
     
-# for i in range(9):
-#     if (i == 5):
-#      break
-#     print("5 X", i+1, "=", 5 * (i+1))
-# print("Done with the loop.")
+for i in range(9):
+    if (i == 5):
+     break
+    print("5 X", i+1, "=", 5 * (i+1))
+print("Done with the loop.")
 
 # continue: exits the iteration
 # skips the rest of the code in the loop and moves to the next iteration.   
