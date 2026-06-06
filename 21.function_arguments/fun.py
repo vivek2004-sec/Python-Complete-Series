a1640599a5fced6abdc189306d1d46d576e1394f
Vivek <vivekkamble762033@gmail.com>
2026-06-06 20:23:28 +0530

    added new message

---
+++ 21.function_arguments/fun.py
@@ -0,0 +1,7 @@
+# Keyword_variable length argument:
+def std_info(**info):
+    print(type(info))
+    print("student: ", info['name'], info['age'], info['branch'])
+    
+
+std_info(name="vivek", age='22', branch='cse')
