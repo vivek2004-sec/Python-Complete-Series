a68dc569d818fbe4b0e24039be556d6b235d32e7
Vivek <vivekkamble762033@gmail.com>
2026-06-20 19:59:58 +0530

    removed the unnecessary part

--- Secret_language/main.py
+++ Secret_language/main.py
@@ -3,34 +3,16 @@
 import random
 import string
 
-code = input("Enter the code: ")
+def random_char() -> str:
+    '''
+    Generate a random string of 3 ASCII letters.
+    
+    >>> import random
+    >>> random.seed(42)
+    >>> random_chars
+    'ZOX'
+    '''
+    return''.join(random.choices(string.ascii_letters, k=3))
 
-def random_chars():
-    return ''.join(random.choices(string.ascii_letters, k=3))
 
-def random_digits():
-    return ''.join(random.choices(string.digits, k=3))
-
-def encode(code):
-    if len(code) >= 3:
-        code = code[1:] + code[0]          
-        code = random_chars() + random_digits() + code + random_digits() + random_chars()  
-    else:
-        code = code[::-1]                   
-        code = random_chars() + random_digits() + code + random_digits() + random_chars()  
-    return code
-
-def decode(code):
-    code = code[6:-6]
-    if len(code) >= 3:
-        code = code[-1] + code[:-1]
-    else:
-        code = code[::-1]
-    return code
-
-encoded = encode(code)        
-decoded = decode(code)  
-
-print(f"Original → {code}")
-print(f"Encoded  → {encoded}")
-print(f"Decoded  → {decoded}")
+print(string.ascii_letters)
