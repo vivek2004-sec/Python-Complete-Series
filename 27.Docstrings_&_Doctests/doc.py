8450f4707a3857987b4349e8c808ee4a2418ff95
Vivek <vivekkamble762033@gmail.com>
2026-06-21 14:14:28 +0530

    added the code

---
+++ 27.Docstrings_&_Doctests/doc.py
@@ -0,0 +1,21 @@
+# import testmod for testing our function 
+from doctest import testmod 
+  
+# define a function to test 
+def factorial(n): 
+    ''' 
+    This function calculates recursively and 
+    returns the factorial of a positive number. 
+    Define input and expected output: 
+    >>> factorial(3) 
+    6 
+    >>> factorial(5) 
+    120 
+    '''
+    if n <= 1: 
+        return 1
+    return n * factorial(n - 1) 
+  
+# call the testmod function 
+if __name__ == '__main__': 
+    testmod(name ='factorial', verbose = True)
