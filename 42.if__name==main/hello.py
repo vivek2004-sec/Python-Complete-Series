6807901b3dedbc2cb70e98d42dc65a4128d7b8cf
Vivek <vivekkamble762033@gmail.com>
2026-06-23 12:26:30 +0530

    made some changes

--- 42.if__name==main/hello.py
+++ 42.if__name==main/hello.py
@@ -1,13 +1,14 @@
-print(__name__)
+# print(__name__)
 
-if __name__=="__main__":
-    print("I am always executed.")
+# if __name__=="__main__":
+#     print("I am always executed.")
 
 
-def encode(code):
-    return code + "123"
+# def encode(code):
+#     return code + "123"
     
-print(__name__)
-if  __name__=="__main__":
-    print(encode("hello"))
+# print(__name__)
+# if  __name__=="__main__":
+#     print(encode("hello"))
 
+import harry
