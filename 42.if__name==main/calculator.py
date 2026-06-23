9c1f120367813610ee2540727a5bb4be15a60531
Vivek <vivekkamble762033@gmail.com>
2026-06-23 12:38:09 +0530

    added new things

--- 42.if__name==main/calculator.py
+++ 42.if__name==main/calculator.py
@@ -7,11 +7,13 @@ def subtract(a: int, b: int) -> int:
     
     return a - b
 
-print("This is simple Calculator!")
-num1 = int(input("Enter the number: "))
-num2 = int(input("Enter the number: "))
-print(f"The sum is: {add(num1, num2)}")
-print(f"The difference  is: {subtract(num1, num2)}")
+
+if __name__== "__main__":
+   print("This is simple Calculator!")
+   num1 = int(input("Enter the number: "))
+   num2 = int(input("Enter the number: "))
+   print(f"The sum is: {add(num1, num2)}")
+   print(f"The difference  is: {subtract(num1, num2)}")
 
 
     
