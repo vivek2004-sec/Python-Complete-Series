96f05d6616f3f69756c1536c843462e432910b14
Vivek <vivekkamble762033@gmail.com>
2026-06-23 12:30:01 +0530

    added new function

--- 42.if__name==main/harry.py
+++ 42.if__name==main/harry.py
@@ -1,11 +1,14 @@
-def welcome():
-    print("Hello You are welcome.")
-
-
-# print(__name__)    
-
-
-# if __name__=="__main__":
-welcome()
+def add(a: int, b: int) -> int:
     
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
     
+    return a - b
+
+print("This is simple Calculator!")
+num1 = int(input("Enter the number: "))
+num2 = int(input("Enter the number: "))
+print(f"The sum is: {add(num1, num2)}")
+print(f"The difference  is: {subtract(num1, num2)}")
