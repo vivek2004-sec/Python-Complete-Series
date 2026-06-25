3a729d7d12577f9007984d0b15c7a3d365c1aa0b
Vivek <vivekkamble762033@gmail.com>
2026-06-25 15:36:59 +0530

    made some changes

--- 48.is_vs_==/main.py
+++ 48.is_vs_==/main.py
@@ -2,7 +2,7 @@ a = [1, 2, 3]
 b = [1, 2, 3]
 
 print(a is b)  # False — different objects, even though values are same
-print(a == b)  
+print(a == b)  # True - defferent objects, but same values.
 
 
 a = 3
