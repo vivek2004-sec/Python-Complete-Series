47074787826d8e0244bba898bd6ec5d9d4ad6d08
Vivek <vivekkamble762033@gmail.com>
2026-06-25 19:11:32 +0530

    added new stuff

--- 43.OS_module/os.py
+++ 43.OS_module/os.py
@@ -26,9 +26,9 @@ for i in folder:
 # os.rename("new_folder", "vivek")
 
 # print(os.path.exists("vivek"))
-print(os.path.isfile("vivek"))
-print(os.path.isdir("vivek"))
-print(os.path.getsize(".idea"))
+# print(os.path.isfile("vivek"))
+# print(os.path.isdir("vivek"))
+# print(os.path.getsize(".idea"))
 
 # print(os.name)
 # print(os.sep)
