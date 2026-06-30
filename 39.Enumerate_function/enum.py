d4df0704c334081e1090404953c2f1d0fe0c1eef
Vivek <vivekkamble762033@gmail.com>
2026-06-30 16:10:07 +0530

    added new message

--- 39.Enumerate_function/enum.py
+++ 39.Enumerate_function/enum.py
@@ -1,3 +1,5 @@
+# The enumerate() function in Python is used to iterate over an iterable while keeping track of both the index and the value.
+
 marks = [60, 56, 45, 55, 67, 23]
 for index, mark in enumerate(marks, start = 2):
     print(index, mark)
