03d9e8af678a5f75373f673c158fdca42c0531ce
Vivek <vivekkamble762033@gmail.com>
2026-07-01 12:11:55 +0530

    added new class methods

--- 50.class/car.py
+++ 50.class/car.py
@@ -11,3 +11,10 @@ class car:
         self.year = year
         self.color = color
         self.for_sale = for_sale
+        
+    def drive(self):
+        print("You drive the car.")
+        
+    def stop(self):
+        print("You stop the car.")
+    
