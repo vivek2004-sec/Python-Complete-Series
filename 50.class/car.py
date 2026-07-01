deee7e7e3088e66d7c1cb65c8fdf62bcf2b6760a
Vivek <vivekkamble762033@gmail.com>
2026-07-01 12:16:13 +0530

    added new things

--- 50.class/car.py
+++ 50.class/car.py
@@ -13,8 +13,8 @@ class car:
         self.for_sale = for_sale
         
     def drive(self):
-        print("You drive the car.")
+        print(f"You drive the {self.model}.")
         
     def stop(self):
-        print("You stop the car.")
+        print(f"You stop the {self.model}.")
     
