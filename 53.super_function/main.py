16ad5b714e20c7f6d6e06d9baceb7dfc423b863f
Vivek <vivekkamble762033@gmail.com>
2026-07-01 23:36:29 +0530

    added new things

--- 53.super_function/main.py
+++ 53.super_function/main.py
@@ -2,6 +2,9 @@ class shape():
     def __init__(self, color, is_filled):
         self.color = color
         self.is_filled = is_filled
+        
+    def describe(self):
+        print(f"It is {self.color} and {"filled" if self.is_filled else "not filled."}")
 
 
 class Circle(shape):
@@ -27,3 +30,6 @@ print(circle.color)
 print(circle.is_filled)
 print(square.color)
 print(square.is_filled)
+
+circle.describe()
+square.describe()
