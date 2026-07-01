9df187c8581e5c5f3e95ac75fc08f2448cf77efc
Vivek <vivekkamble762033@gmail.com>
2026-07-01 23:14:53 +0530

    added new super classes

---
+++ 53.super_function/main.py
@@ -0,0 +1,17 @@
+class Circle:
+   def __init__(self, color, is_filled, radius):
+       self.color = color
+       self.is_filled = is_filled
+       self.radius = radius
+          
+class Square: 
+    def __init__(self, color, is_filled, edges):
+        self.color = color
+        self.is_filled = is_filled
+        self.edges = edges
+
+class Triangle:
+    def __init__(self, color, is_filled, edges):
+        self.color = color
+        self.is_filled = is_filled
+        self.edges = edges
