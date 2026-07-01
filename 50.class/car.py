c890b26f0478bfb0f772595a09907213b92d2643
Vivek <vivekkamble762033@gmail.com>
2026-07-01 12:24:45 +0530

    added new things

--- 50.class/car.py
+++ 50.class/car.py
@@ -18,3 +18,5 @@ class car:
     def stop(self):
         print(f"You stop the {self.color} {self.model}.")
     
+    def describe(self):
+        print(f"car details:\n year = {self.year}\n color = {self.color}\n status = {self.for_sale}")
