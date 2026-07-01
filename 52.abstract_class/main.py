f04b7373c9013d98a4364699a752aa186b808b55
Vivek <vivekkamble762033@gmail.com>
2026-07-01 20:07:40 +0530

    added new things

---
+++ 52.abstract_class/main.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    
+    @abstractmethod 
+    def go(self):
+        pass
+    
+    @abstractmethod
+    def stop(self):
+        pass
+    
+    
+class Car(Vehicle):
+    
+    def go(self):
+        print("You drive the car.")
+        
+    def stop(self):
+        print("You stop the car.")
+    
+
+car = Car()
+    
