3c74c880d1051329766b45926609f4a83a9d213a
Vivek <vivekkamble762033@gmail.com>
2026-07-01 11:59:13 +0530

    added new things

--- 50.class/class.py
+++ 50.class/class.py
@@ -44,8 +44,13 @@ class car:
         self.for_sale = for_sale
         
 car1 = car("mustang", 2022, "black", False)
+car2 = car("corvette", 2024, "blue", True)
 print(car1)
 print(car1.model) # (.) is know attribute access operator.
-print(car1.year) 
 print(car1.color) 
+print(car1.year) 
 print(car1.for_sale) 
+print(car2.model) 
+print(car2.color) 
+print(car2.year) 
+print(car2.for_sale) 
