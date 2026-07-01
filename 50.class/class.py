ba866b5a91fd00ebfecf9c410468d21ea43cfb51
Vivek <vivekkamble762033@gmail.com>
2026-07-01 12:31:18 +0530

    saved the changes

--- 50.class/class.py
+++ 50.class/class.py
@@ -31,16 +31,3 @@ print(a.pincode)
 
 
 
-from car import car
-        
-car1 = car("mustang", 2022, "black", False)
-car2 = car("corvette", 2024, "blue", True)
-print(car1)
-print(car1.model) # (.) is know attribute access operator.
-print(car1.color) 
-print(car1.year) 
-print(car1.for_sale) 
-print(car2.model) 
-print(car2.color) 
-print(car2.year) 
-print(car2.for_sale) 
