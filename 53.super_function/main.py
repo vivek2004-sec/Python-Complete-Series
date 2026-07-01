bf6abc01f65138d35bfddcf7146a9640bcda88c1
Vivek <vivekkamble762033@gmail.com>
2026-07-01 23:18:57 +0530

    added new things

--- 53.super_function/main.py
+++ 53.super_function/main.py
@@ -1,16 +1,20 @@
-class Circle:
+class shape():
+    pass
+
+
+class Circle(shape):
    def __init__(self, color, is_filled, radius):
        self.color = color
        self.is_filled = is_filled
        self.radius = radius
           
-class Square: 
+class Square(shape): 
     def __init__(self, color, is_filled, width):
         self.color = color
         self.is_filled = is_filled
         self.width = width
 
-class Triangle:
+class Triangle(shape):
     def __init__(self, color, is_filled, width, height):
         self.color = color
         self.is_filled = is_filled
