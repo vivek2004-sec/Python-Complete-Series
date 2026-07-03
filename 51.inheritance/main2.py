84dfa174a373948f8c13b7a35459f3da74741407
Vivek <vivekkamble762033@gmail.com>
2026-07-03 19:35:59 +0530

    added new class

---
+++ 51.inheritance/main2.py
@@ -0,0 +1,17 @@
+# single Inheritance:
+
+class vehicle:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+        
+        
+    def drive(self):
+        print("You can drive.")
+        
+    def stop(self):
+        print("You can stop.")
+        
+        
+bike = vehicle("kawasaki", 200)
+print(bike)
