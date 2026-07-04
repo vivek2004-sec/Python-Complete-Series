5ef37ea79acbe972ff0ba6cbc1c3a7606c45c2e7
Vivek <vivekkamble762033@gmail.com>
2026-07-04 16:21:20 +0530

    added new stuff

--- numpy/4.arithmetic/main.py
+++ numpy/4.arithmetic/main.py
@@ -1,11 +1,15 @@
 import numpy as np
 
-# Scalar arithmetic: scalar means single value.
-
 array = np.array([1, 2, 3])
+
+# Scalar arithmetic: scalar means single value.
 print(array + 1)
 print(array - 2)
 print(array * 3)
 print(array ** 2)
 print(array / 2)
 print(array // 2)
+
+# Vectorized maths functions:
+
+print(np.sqrt(array))
