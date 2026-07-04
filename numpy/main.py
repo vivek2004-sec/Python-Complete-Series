e1d3548723345e95eeec81e45c84b54982d1226e
Vivek <vivekkamble762033@gmail.com>
2026-07-04 12:17:30 +0530

    added new stuff

--- numpy/main.py
+++ numpy/main.py
@@ -1,4 +1,10 @@
 import numpy as np
 
+# my_list = [1, 2, 3, 4]
 
-print(np.__version__)
+# my_list = my_list * 2  # output: [1, 2, 3, 4, 1, 2, 3, 4]
+# print(my_list)
+
+array = np.array([1, 2, 3, 4])
+print(array)
+print(type(array))
