233c721208dc08bd021d0e5178314dc9e30801f2
Vivek <vivekkamble762033@gmail.com>
2026-07-04 16:18:54 +0530

    added new stuff

--- numpy/4.arithmetic/main.py
+++ numpy/4.arithmetic/main.py
@@ -1,3 +1,11 @@
 import numpy as np
 
 # Scalar arithmetic: scalar means single value.
+
+array = np.array([1, 2, 3])
+print(array + 1)
+print(array - 2)
+print(array * 3)
+print(array ** 2)
+print(array / 2)
+print(array // 2)
