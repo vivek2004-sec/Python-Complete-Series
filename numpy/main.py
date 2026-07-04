3c864563c793e9595555ed0094c14ef03de65657
Vivek <vivekkamble762033@gmail.com>
2026-07-04 12:18:39 +0530

    added new stuff

--- numpy/main.py
+++ numpy/main.py
@@ -6,5 +6,7 @@ import numpy as np
 # print(my_list)
 
 array = np.array([1, 2, 3, 4])
-print(array)
-print(type(array))
+print(type(array)) # <class 'numpy.ndarray'>
+
+array = array * 2
+print(array) # output: [2 4 6 8]
