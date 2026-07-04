50ff24bdb0414a50da73f404c7ef4300579b0a7f
Vivek <vivekkamble762033@gmail.com>
2026-07-04 16:24:50 +0530

    added new array functions

--- numpy/4.arithmetic/main.py
+++ numpy/4.arithmetic/main.py
@@ -1,15 +1,19 @@
 import numpy as np
 
-array = np.array([1, 2, 3])
+# array = np.array([1, 2, 3])
 
 # Scalar arithmetic: scalar means single value.
-print(array + 1)
-print(array - 2)
-print(array * 3)
-print(array ** 2)
-print(array / 2)
-print(array // 2)
+# print(array + 1)
+# print(array - 2)
+# print(array * 3)
+# print(array ** 2)
+# print(array / 2)
+# print(array // 2)
 
 # Vectorized maths functions:
 
+array = np.array([1.23, 2.005, 3.987])
 print(np.sqrt(array))
+print(np.round(array))
+print(np.floor(array))
+print(np.ceil(array))
