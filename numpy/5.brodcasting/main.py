4ff086061dd0a5eda46879ef0ad99ea9912e03c1
Vivek <vivekkamble762033@gmail.com>
2026-07-05 15:07:44 +0530

    added new stuff

--- numpy/5.brodcasting/main.py
+++ numpy/5.brodcasting/main.py
@@ -1,10 +1,13 @@
 import numpy as np
 
-array1 = np.array([[1, 2, 3, 4]])
+array1 = np.array([[1, 2, 3, 4], 
+                   [5, 6, 7, 8],
+                   [9, 10, 11, 12],
+                   [13, 14, 15, 16]])
 array2 = np.array([[1], [2], [3], [4]])
 
-# print(array1.shape)
-# print(array2.shape)
+print(array1.shape)
+print(array2.shape)
 
 array3 = array1 * array2
 print(array3)
