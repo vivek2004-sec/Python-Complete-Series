cc367a2e32557856c46d520e7c4a46d7e5e1d7fb
Vivek <vivekkamble762033@gmail.com>
2026-07-05 15:23:25 +0530

    added new stuff

--- numpy/5.brodcasting/main.py
+++ numpy/5.brodcasting/main.py
@@ -1,16 +1,24 @@
 import numpy as np
 
-array1 = np.array([[1, 2, 3, 4], 
-                   [5, 6, 7, 8],
-                   [9, 10, 11, 12],
-                   [13, 14, 15, 16]])
-array2 = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]])
+# array1 = np.array([[1, 2, 3, 4], 
+#                    [5, 6, 7, 8],
+#                    [9, 10, 11, 12],
+#                    [13, 14, 15, 16]])
+# array2 = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]])
+
+# print(array1.shape)
+# print(array2.shape)
+
+# array3 = array1 * array2
+# print(array3)
+# print(array3.ndim)
+# print(array3.shape)
+# print(array3.size)
+
+array1 = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+array2 = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
 
 print(array1.shape)
 print(array2.shape)
-
 array3 = array1 * array2
 print(array3)
-print(array3.ndim)
-print(array3.shape)
-print(array3.size)
