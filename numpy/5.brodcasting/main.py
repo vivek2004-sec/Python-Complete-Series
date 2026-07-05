acc2d3c6da028b285431599b9ca49fa0dcee948d
Vivek <vivekkamble762033@gmail.com>
2026-07-05 15:11:49 +0530

    added new stuff

--- numpy/5.brodcasting/main.py
+++ numpy/5.brodcasting/main.py
@@ -4,7 +4,7 @@ array1 = np.array([[1, 2, 3, 4],
                    [5, 6, 7, 8],
                    [9, 10, 11, 12],
                    [13, 14, 15, 16]])
-array2 = np.array([[1], [2], [3], [4]])
+array2 = np.array([[1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]])
 
 print(array1.shape)
 print(array2.shape)
