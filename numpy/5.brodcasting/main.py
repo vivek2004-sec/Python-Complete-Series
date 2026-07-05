6d170a8f3be01c8142464ee648eaffe6890bb728
Vivek <vivekkamble762033@gmail.com>
2026-07-05 15:00:47 +0530

    added new stuff

--- numpy/5.brodcasting/main.py
+++ numpy/5.brodcasting/main.py
@@ -3,5 +3,11 @@ import numpy as np
 array1 = np.array([[1, 2, 3, 4]])
 array2 = np.array([[1], [2], [3], [4]])
 
-print(array1.shape)
-print(array2.shape)
+# print(array1.shape)
+# print(array2.shape)
+
+array3 = array1 * array2
+print(array3)
+print(array3.ndim)
+print(array3.shape)
+print(array3.size)
