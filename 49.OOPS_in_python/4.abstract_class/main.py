59c5506667f884bacee5b7236fcc0e2c4508a487
Vivek <vivekkamble762033@gmail.com>
2026-07-06 17:39:02 +0530

    added this to oops

---
+++ 49.OOPS_in_python/4.abstract_class/main.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+    
+    @abstractmethod 
+    def go(self):
+        pass
+    
+    @abstractmethod
+    def stop(self):
+        pass
+    
+    
+class Car(Vehicle):
+    
+    def go(self):
+        print("You drive the car.")
+        
+    def stop(self):
+        print("You stop the car.")
+    
+class Motorcycle(Vehicle):
+    def __init__(self, name):
+        self.name = name
+    
+    def go(self):
+        print("You ride the motorcycle.")
+        
+    def stop(self):
+        print("You stop the motorcycle.")
+    
+    
+motorcycle = Motorcycle("Bullet")
+print(motorcycle.name)
+motorcycle.go()
+motorcycle.stop()
+
+
+class Boat(Vehicle):
+    def go(self):
+        print("You sail the boat.")
+        
+    def stop(self):
+        print("You anchor the boat.")
+
+boat = Boat()
+boat.go()
+boat.stop()
