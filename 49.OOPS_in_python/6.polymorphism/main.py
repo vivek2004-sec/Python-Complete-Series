1823b9d045a38317ff68d33f6424cbb0921b27bd
Vivek <vivekkamble762033@gmail.com>
2026-07-06 18:00:43 +0530

    added new stuff

---
+++ 49.OOPS_in_python/6.polymorphism/main.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+
+
+class shape:
+    
+    @abstractmethod
+    def area(self):
+        pass
+    
+    
+class Circle(shape):
+    def __init__(self, radius):
+        self.radius = radius
+    
+    def area(self):
+        return 3.1 * self.radius ** 2
+
+class Square(shape):
+    def __init__(self, side):
+        self.side = side
+    
+    def area(self):
+        return self.side ** 2
+
+class Triangle(shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+        
+    def area(self):
+       return 0.5 * self.base * self.height
+
+
+shapes = [Circle(3), Square(5), Triangle(7, 9)]
+
+for shape in shapes:
+    print(shape.area())
