d0616e722d643e4131f4e4a15bba5e9163cbc36f
Vivek <vivekkamble762033@gmail.com>
2026-07-22 15:19:38 +0530

    added duckt typing

---
+++ 49.OOPS_in_python/7.duck_typing/main.py
@@ -0,0 +1,23 @@
+class Animal:
+    alive = True
+    
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+    
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+        
+class Car:
+    
+    alive = False
+    def speak(self):
+        print("Honk!")
+        
+        
+animals = [Dog(), Cat(), Car()]
+
+for animal in animals:
+    animal.speak()
+    print(animal.alive)
