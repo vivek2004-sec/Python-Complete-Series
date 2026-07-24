91e181b8497ae2d3847f80b475f07a03676147cb
Vivek <vivekkamble762033@gmail.com>
2026-07-24 19:59:43 +0530

    added new things

--- 49.OOPS_in_python/9.composition/main.py
+++ 49.OOPS_in_python/9.composition/main.py
@@ -1,37 +1,33 @@
-# class Engine:
-#     def __init__(self):
-#         self.status = "running"
+class Engine:
+    def __init__(self):
+        self.status = "running"
 
-# class Car:
-#     def __init__(self):
-#         self.engine = Engine()   # Car creates its own Engine internally
+class Car:
+    def __init__(self):
+        self.engine = Engine()   # Car creates its own Engine internally
 
-# car = Car()
-# del car   # when car is destroyed, its engine goes with it — no external reference
-
-
-# class Engine:
-#     def __init__(self, horse_power):
-#         self.horse_power = horse_power
-
-# class Wheel:
-#    def __init__(self, size):
-#        self.size = size
-
-# class Car:
-#     def __init__(self, make, model, horse_power, wheel_size):
-#         self.make = make
-#         self.model = model
-#         self.engine = Engine(horse_power)
-#         self.wheels = [Wheel(wheel_size) for wheel in range(4)]
-        
-
-# car = Car(make="Ford", model="Mustang", )
+car = Car()
+del car   # when car is destroyed, its engine goes with it — no external reference
 
 
-import calendar
+class Engine:
+    def __init__(self, horse_power):
+        self.horse_power = horse_power
 
-year = int(input("Enter year: "))
-month = int(input("Enter month: "))
+class Wheel:
+   def __init__(self, size):
+       self.size = size
 
-print("\n", calendar.month(year, month))
+class Car:
+    def __init__(self, make, model, horse_power, wheel_size):
+        self.make = make
+        self.model = model
+        self.engine = Engine(horse_power)
+        self.wheels = [Wheel(wheel_size) for wheel in range(4)]
+    
+    def display_car(self):
+        return f"{self.make} {self.model} {self.engine.horse_power}(hp) {self.wheels}"
+
+car = Car(make="Ford", model="Mustang", horse_power=500, wheel_size=18 )
+print(car.display_car())
+
