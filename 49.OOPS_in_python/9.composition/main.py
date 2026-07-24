83e372ec4df9bbb65fd7fe40c983e350f4739ff9
Vivek <vivekkamble762033@gmail.com>
2026-07-24 19:52:34 +0530

    added new calender project

--- 49.OOPS_in_python/9.composition/main.py
+++ 49.OOPS_in_python/9.composition/main.py
@@ -1,10 +1,37 @@
-class Engine:
-    def __init__(self):
-        self.status = "running"
+# class Engine:
+#     def __init__(self):
+#         self.status = "running"
 
-class Car:
-    def __init__(self):
-        self.engine = Engine()   # Car creates its own Engine internally
+# class Car:
+#     def __init__(self):
+#         self.engine = Engine()   # Car creates its own Engine internally
 
-car = Car()
-del car   # when car is destroyed, its engine goes with it — no external reference
+# car = Car()
+# del car   # when car is destroyed, its engine goes with it — no external reference
+
+
+# class Engine:
+#     def __init__(self, horse_power):
+#         self.horse_power = horse_power
+
+# class Wheel:
+#    def __init__(self, size):
+#        self.size = size
+
+# class Car:
+#     def __init__(self, make, model, horse_power, wheel_size):
+#         self.make = make
+#         self.model = model
+#         self.engine = Engine(horse_power)
+#         self.wheels = [Wheel(wheel_size) for wheel in range(4)]
+        
+
+# car = Car(make="Ford", model="Mustang", )
+
+
+import calendar
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+print("\n", calendar.month(year, month))
---
+++ project/calender.py
@@ -0,0 +1,10 @@
+import calendar
+from datetime import date
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+print("\n", calendar.month(year, month))
+
+
+
