a4592cc40a01d5ddcb408fb60216d8e7c6f2c11c
Vivek <vivekkamble762033@gmail.com>
2026-07-26 12:27:53 +0530

    added new nested class things

---
+++ 49.OOPS_in_python/11.nested_class/main.py
@@ -0,0 +1,39 @@
+
+# class Company:
+#     class Employee:
+#         print("this is the first company.")
+    
+# class NonProfit:  
+#     class Employee:
+#         print("this is the second company.")
+
+
+
+class Company:
+    class Employee:
+        def __init__(self, name, position):
+            self.name = name
+            self.position = position
+    
+        def get_detail(self):
+             return f"{self.name} {self.position}"
+    
+    
+    def __init__(self, company_name):
+            self.company_name = company_name
+            self.employees = []
+            
+        
+    def add_employee(self, name, position):
+            new_employee = self.Employee(name, position)
+            self.employees.append(new_employee)
+            
+    def list_employees(self):
+            return [employee.get_detail() for employee in self.employees]
+        
+company = Company("Microsoft")
+company.add_employee("vivek", "SDE II")
+company.add_employee("rishi", "SDE II")
+company.add_employee("vikrant", "SDE II")
+
+print(company.list_employees())
---
+++ 49.OOPS_in_python/11.nested_class/nested.md
@@ -0,0 +1,5 @@
+# Nested classes: A class defined within another class.
+
+Allows you to group classes which are closely related.
+Encapsulates private details that aren't relavant outside of the outer class.
+Keeps the namespace clean; reduces the possiblity of naming conflicts.
