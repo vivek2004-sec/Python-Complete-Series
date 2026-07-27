305c4c491afef5763c339a7717b7b2fbcc11ae1a
Vivek <vivekkamble762033@gmail.com>
2026-07-27 15:23:13 +0530

    added new stuff

--- 49.OOPS_in_python/12.staticmethods/main.py
+++ 49.OOPS_in_python/12.staticmethods/main.py
@@ -8,3 +8,22 @@ class cal:
         """
 sum = cal.add(3, 4)
 print(sum)
+
+'''
+@staticmethod makes add() independent of class object.
+We directly called the add() method using the class name.
+'''
+class Check:
+    @staticmethod
+    def is_even(n):
+        return n % 2 == 0
+    
+    
+    def find(is_even):
+        if is_even == True:
+                print("The number is even.")
+        else:
+            print("The number is odd.")
+
+print(Check.is_even(9))
+Check.find()
