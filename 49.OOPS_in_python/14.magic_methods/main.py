62db0b985b7f1affded19dad06011b47b3b1a772
Vivek <vivekkamble762033@gmail.com>
2026-07-28 20:23:04 +0530

    added new things

--- 49.OOPS_in_python/14.magic_methods/main.py
+++ 49.OOPS_in_python/14.magic_methods/main.py
@@ -14,13 +14,23 @@ class Book:
     
     def __lt__(self, other):
         return self.num_pages < other.num_pages
+    
+    def __gt__(self, other):
+        return self.num_pages < other.num_pages
+    
+    
+    def __add__(self, other):
+        return self.num_pages + other.num_pages
+    
 book1 = Book("The Hobbit", "J.R.R. Tolkien", 300)
 book2 = Book("The Harry Potter", "J.K.Rowlings", 320)
 book3 = Book("It", "Stephen King", 400)
-book4 = Book("It", "Stephen King", 300)
+
 print(book1)
 print(book2)
 print(book3)
 
-print(book3 == book4)
-print(book3 < book4)
+# print(book3 == book1)
+print(book3 < book1)
+print(book3 > book1)
+print(book3 + book1)
