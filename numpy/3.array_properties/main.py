cb963533ce4d6382b44c24a68ad9adbd1b38ef21
Vivek <vivekkamble762033@gmail.com>
2026-08-02 18:28:09 +0530

    added new things

--- numpy/3.array_properties/main.py
+++ numpy/3.array_properties/main.py
@@ -5,3 +5,14 @@ int_num = np.array([1.2, 2.3, 4.5])
 num = int_num.astype(int)
 print(num)
 print(num.dtype)
+
+#  Array operations
+
+array = np.array([1, 2, 3, 4, 5])
+print(array + 1)
+print(array - 1)
+print(array * 1)
+print(array / 1)
+print(array ** 1)
+print(array // 1)
+print(array % 2)
