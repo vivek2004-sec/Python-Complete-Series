971b7fa3c23a747066ad13d2bb1db21dfd39b44f
Vivek <vivekkamble762033@gmail.com>
2026-08-02 17:41:09 +0530

    added new things

--- numpy/2.multidimensional_array/main.py
+++ numpy/2.multidimensional_array/main.py
@@ -9,35 +9,41 @@ import numpy as np
 # print(array.size)    # 2
 
 
-array = np.array([1, 2, 3, 4])
-print(array.ndim)             # rank 1
-print(array.dtype)            # int64
+# array = np.array([1, 2, 3, 4])
+# print(array.ndim)             # rank 1
+# print(array.dtype)            # int64
 
-array = np.array([['A', 'B', 'C'], 
-                  ['D', 'E', 'F'], 
-                  ['G', 'H', 'I']])
-print(array.ndim)                   # rank 2
-print(array.shape)                  
-print(array.size)                  
-print(array.dtype)                  
-print("Itemsize: ",array.itemsize)                  
+# array = np.array([['A', 'B', 'C'], 
+#                   ['D', 'E', 'F'], 
+#                   ['G', 'H', 'I']])
+# print(array.ndim)                   # rank 2
+# print(array.shape)                  
+# print(array.size)                  
+# print(array.dtype)                  
+# print("Itemsize: ",array.itemsize)                  
 
 
-array = np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']],
-                  [['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R']],
-                  [['S', 'T', 'U'], ['V', 'W', 'X'], ['Y', 'Z', '$']]] ) 
+# array = np.array([[['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']],
+#                   [['J', 'K', 'L'], ['M', 'N', 'O'], ['P', 'Q', 'R']],
+#                   [['S', 'T', 'U'], ['V', 'W', 'X'], ['Y', 'Z', '$']]] ) 
 
-print(array.ndim)                                                             # rank 3
-print(array.shape)                                                            # (3, 3, 3)
-print(array.size)                                                             # 27
-print(array[0][0][0])      # Chain indexing                                   # A
-print(array[0, 0, 0])      # Multidimensional indexing                        # A
-print(array[0, 0, 1])   
-print(array[0, 0, 2])   
-print(array[0, 1, 0])   
-print(array[2, 2, 2])   
-# print(array[layer-index, row-index, column-index])   
+# print(array.ndim)                                                             # rank 3
+# print(array.shape)                                                            # (3, 3, 3)
+# print(array.size)                                                             # 27
+# print(array[0][0][0])      # Chain indexing                                   # A
+# print(array[0, 0, 0])      # Multidimensional indexing                        # A
+# print(array[0, 0, 1])   
+# print(array[0, 0, 2])   
+# print(array[0, 1, 0])   
+# print(array[2, 2, 2])   
+# # print(array[layer-index, row-index, column-index])   
 
-#  Create a word 'vivek'
+# #  Create a word 'vivek'
 
-print(array[2, 1, 0] + array[0, 2, 2 ] + array[2, 1, 0] + array[0, 1, 1]+ array[1, 0, 1])
+# print(array[2, 1, 0] + array[0, 2, 2 ] + array[2, 1, 0] + array[0, 1, 1]+ array[1, 0, 1])
+
+
+# Array sorting 
+
+num = np.array([1, 32, 3, 4, 55, 5, 12])
+print(np.sort(num))
