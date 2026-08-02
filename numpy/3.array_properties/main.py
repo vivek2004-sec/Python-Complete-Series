7cafdecc9ecf2c45cf1674eb6def97b0ff80b28b
Vivek <vivekkamble762033@gmail.com>
2026-08-02 18:32:53 +0530

    added new stuff

--- numpy/3.array_properties/main.py
+++ numpy/3.array_properties/main.py
@@ -8,12 +8,20 @@ import numpy as np
 
 #  Array operations
 
-array = np.array([[1, 2, 3, 4, 5],
-                  [6, 7, 8, 9, 10]])
-print(array + 1)
-print(array - 1)
-print(array * 1)
-print(array / 1)
-print(array ** 1)
-print(array // 1)
-print(array % 2)
+# array = np.array([[1, 2, 3, 4, 5],
+#                   [6, 7, 8, 9, 10]])
+# print(array + 1)
+# print(array - 1)
+# print(array * 1)
+# print(array / 1)
+# print(array ** 1)
+# print(array // 1)
+# print(array % 2)
+
+
+# Aggregation in numpy
+
+array = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]])
+print(np.sum(array))
+print(np.mean(array))
+print(np.median(array))
