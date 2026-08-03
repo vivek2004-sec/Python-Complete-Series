c60f5c06b7ce10e0ffe150f085cf2de17e262d10
Vivek <vivekkamble762033@gmail.com>
2026-08-03 19:05:47 +0530

    added new things

--- numpy/8.advanced_numpy/main.py
+++ numpy/8.advanced_numpy/main.py
@@ -7,3 +7,8 @@ for 2D array:
 axis = 0 -> row wise
 axis = 1 -> column wise
 """
+import numpy as np
+
+array = np.array([10, 20, 30, 40, 50, 60])
+new_array = np.insert(array, 2, 35, axis=None)
+print(new_array)
