52c8fab3445a717ec3359540f189453870e14013
Vivek <vivekkamble762033@gmail.com>
2026-08-03 19:36:51 +0530

    added new stuff

---
+++ numpy/8.advanced_numpy/stacking.py
@@ -0,0 +1,13 @@
+'''
+vstack() : vertically 
+hstack() : horizontally
+'''
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+arr3 = np.vstack((arr1, arr2))
+arr4 = np.hstack((arr1, arr2))
+print(arr3)
+print(arr4)
