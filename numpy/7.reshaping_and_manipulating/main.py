ff88b7de5eced6af05608fe3f448e8a8212f14d0
Vivek <vivekkamble762033@gmail.com>
2026-08-03 18:47:34 +0530

    added new file of reshaping and manipulation of data

---
+++ numpy/7.reshaping_and_manipulating/main.py
@@ -0,0 +1,13 @@
+#  Reshaping = changing the dimensions of the array without modifying the data inside it.
+#  The number of elements in the array will still be the same even after reshaping the array.
+#  reshaping does not create a copy it creates a view. 
+"""
+.reshape(rows, columns)   
+    
+"""
+
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5, 6])
+reshaped_arr = array.reshape(2, 3)
+print(reshaped_arr)
