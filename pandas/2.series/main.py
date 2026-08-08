c73386f54db3136592928a905797a1d9bad4185d
Vivek <vivekkamble762033@gmail.com>
2026-08-08 18:42:24 +0530

    added new things

--- pandas/2.series/main.py
+++ pandas/2.series/main.py
@@ -4,28 +4,31 @@ import pandas as pd
 #  For list
 data = [100, 101, 104]
 
-series = pd.Series(data)
+series = pd.Series(data, index=['a', 'b', 'c'])
+print(series)
+print(series.loc['a']) # loc = location by label
+print(series.loc['c']) # loc = location by label
+series.loc['c'] = 200
 print(series)
 
+# # For list 
+# names = ['vivek', 'sujal', 'sai', 'sahil', 'sushant', 'samarth']
 
-# For list 
-names = ['vivek', 'sujal', 'sai', 'sahil', 'sushant', 'samarth']
-
-std = pd.Series(names)
-print(std)
+# std = pd.Series(names)
+# print(std)
 
 
-# For dictionary
-students = {
-    'vivek': 1.2,
-    'sujal': 3.0
-}
+# # For dictionary
+# students = {
+#     'vivek': 1.2,
+#     'sujal': 3.0
+# }
 
-obj = pd.Series(students)
-print(obj)
+# obj = pd.Series(students)
+# print(obj)
 
 # For tuple
 fruits = ('apple', 'mango', 'grapes', 'banana')
 
 shakes = pd.Series(fruits)
-print(shakes)
+print(shakes.loc[0])
