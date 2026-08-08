fbab69db4ddb8ecac3c23335e96c53efe46a5834
Vivek <vivekkamble762033@gmail.com>
2026-08-08 18:32:06 +0530

    added new things

--- pandas/2.series/main.py
+++ pandas/2.series/main.py
@@ -1,16 +1,21 @@
 import pandas as pd 
 
+
+#  For list
 data = [100, 101, 104]
 
 series = pd.Series(data)
 print(series)
 
 
+# For list 
 names = ['vivek', 'sujal', 'sai', 'sahil', 'sushant', 'samarth']
 
 std = pd.Series(names)
 print(std)
 
+
+# For dictionary
 students = {
     'vivek': 1.2,
     'sujal': 3.0
@@ -19,7 +24,7 @@ students = {
 obj = pd.Series(students)
 print(obj)
 
-
+# For tuple
 fruits = ('apple', 'mango', 'grapes', 'banana')
 
 shakes = pd.Series(fruits)
