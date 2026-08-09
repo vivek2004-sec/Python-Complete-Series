f7b234cafe377220237e6ad4ba95a80551430d4f
Vivek <vivekkamble762033@gmail.com>
2026-08-09 12:56:53 +0530

    added new stuff

--- pandas/4.dataframes/main.py
+++ pandas/4.dataframes/main.py
@@ -4,12 +4,39 @@ import pandas as pd
 #  Dataframes = A tabular data structure with row and columns. (2-Dimensional) Similar to an Excel spreadsheet.
 
 
-data = {
-    "Name": ["Ash", "Misty", "Brock"],
-    "Age": [12, 12, 12], 
-    "Companions": ["pikachu", "togefree", 'Rocky']
+# data = {
+#     "Name": ["Ash", "Misty", "Brock"],
+#     "Age": [12, 12, 12], 
+#     "Companions": ["pikachu", "togefree", 'Rocky']
+# }
+
+# df = pd.DataFrame(data, index=[1, 2, 3])
+
+# # Add a new column 
+# df['job'] = ['Trainer', 'gym master', 'gym master']
+
+# # Add a new row
+# new_row = pd.DataFrame({"Name": "Sandy", "Age": 20, "Companions": 'balbasaur', 'job':"trainer"}, index=[4])
+# df = pd.concat([df, new_row])
+# print(df)
+
+
+# print(df.loc[3])
+# print(df.iloc[0])
+
+
+students = {
+    "name": ['vivek', 'sujal', 'aditya', 'sahil'],
+    "age": [22, 22, 22, 22], 
+    'branch': ['cse', 'chemical', 'cse', 'civil']
 }
 
-df = pd.DataFrame(data, index=[1, 2, 3])
-print(df.loc[3])
-print(df.iloc[0])
+df = pd.DataFrame(students, index=[1, 2, 3, 4])
+# Adding a column
+df['city'] = ['kagal', 'kagal', 'kagal', 'kagal']
+# Adding  Rows
+new_row = pd.DataFrame([{"name": 'sai', 'age': 22, 'branch': 'commerce', 'city': 'kagal'},
+                       {"name": 'sainath', 'age': 22, 'branch': 'commerce', 'city': 'kagal'},
+                       {"name": 'sushant', 'age': 22, 'branch': 'bio', 'city': 'kolhapur'}], index=[5, 6, 7])
+df = pd.concat([df, new_row])
+print(df)
