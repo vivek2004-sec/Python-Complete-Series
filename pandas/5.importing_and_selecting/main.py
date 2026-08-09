8164141a8fcee2aecdc1b68548ca427d0c5fac60
Vivek <vivekkamble762033@gmail.com>
2026-08-09 13:14:06 +0530

    added new stuff

---
+++ pandas/5.importing_and_selecting/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# csv = comma -seperated values
+# json = Javascript object Notaion
+
+# df = pd.read_csv('pandas/5.importing/pokemon.csv')
+# print(df.to_string())
+
+# df = pd.read_json('pandas/5.importing/pokemon.json')
+# print(df.to_string())
+
+
+# Selection
+
+# 1.selection by column
+
+df = pd.read_csv('pandas/5.importing_and_selecting/pokemon.csv')
+# print(df["Name"].to_string())
+print(df["Height"].to_string())
