e25fbc14df5b4768ba12f919e33a02bb58846c8d
Vivek <vivekkamble762033@gmail.com>
2026-08-09 23:14:04 +0530

    added new things

--- pandas/7.aggregation/main.py
+++ pandas/7.aggregation/main.py
@@ -4,4 +4,24 @@ import pandas as pd
                     #   groupby() function
                     
 df = pd.read_csv('pandas/7.aggregation/pokemon.csv')
-print(df.mean(numeric_only=True))
+
+#  Whole DataFrame
+# print(df.mean(numeric_only=True))
+# print(df.sum(numeric_only=True))
+# print(df.min(numeric_only=True))
+# print(df.max(numeric_only=True))
+# print(df.count())
+
+# For a single Column 
+
+# print(df["Height"].mean())
+# print(df["Height"].sum())
+# print(df["Height"].min())
+# print(df["Height"].max())
+# print(df["Height"].count())
+
+group = df.groupby("Type1")
+# print(group["Name"].count())
+# print(group["Height"].sum())
+# print(group["Height"].min())
+print(group["Height"].max())
---
+++ pandas/8.data_cleaning/main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+# Data Cleaning : the process of fixing/removing : 
+#                 incomplete, incorrect, or irrevelant data.
+#                  ~75% of work done with pandas is data cleaning. 
+
+df = pd.read_csv('pandas/8.data_cleaning/pokemon.csv')
+
+# 1. Drop a column
+# df = df.drop(columns=["Legendary", 'Height', 'W
+
+# 2. Handle Missing values
+# df = df.dropna(subset=["Type1", "Type2"])
+# df = df.fillna({"Type2": "NONE"})
+
+# 3. Fix inconsistent values
+# df["Type1"] = df['Type1'].replace({"Grass": "GRASS"})
+
+# 4. Standardize Text
+# df['Name'] = df['Name'].str.lower()
+
+# 5. changing data types 
+
+df["Legendary"] = df['Legendary'].astype(bool)
+print(df.to_string())
