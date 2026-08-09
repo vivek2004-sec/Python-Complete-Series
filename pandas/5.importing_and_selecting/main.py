9ea5510da9433e5576eba545a25aff26d756b022
Vivek <vivekkamble762033@gmail.com>
2026-08-09 17:40:51 +0530

    added new things

--- pandas/5.importing_and_selecting/main.py
+++ pandas/5.importing_and_selecting/main.py
@@ -14,7 +14,20 @@ import pandas as pd
 
 # 1.selection by column
 
-df = pd.read_csv('pandas/5.importing_and_selecting/pokemon.csv')
+df = pd.read_csv('pandas/5.importing_and_selecting/pokemon.csv', index_col='Name')
 # print(df["Name"].to_string())
 # print(df["Height"].to_string())
 # print(df["Weight"].to_string())
+# print(df[['Name', 'Weight', 'Height']].to_string())
+
+# 2. Selction by rows
+# print(df.loc['Charizard':"Blastoise", ["Height", "Weight"]])
+# print(df.iloc[0:15:2, 0:3])
+
+pokemon = input("Enter pokemon name: ")
+
+try:
+    print(df.loc[pokemon])
+    
+except KeyError:
+    print(f"{pokemon} not found.")
