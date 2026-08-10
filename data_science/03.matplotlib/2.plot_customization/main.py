558f8158fd4fda9ff6d60efb312164fa0e17fcee
Vivek <vivekkamble762033@gmail.com>
2026-08-10 11:10:12 +0530

    added new stuff

--- data_science/03.matplotlib/2.plot_customization/main.py
+++ data_science/03.matplotlib/2.plot_customization/main.py
@@ -3,7 +3,24 @@ import numpy as np
 
 
 x = np.array([2025, 2026, 2027, 2028])
-y = np.array([10, 15, 20, 15])
+y1 = np.array([10, 15, 20, 15])
+y2 = np.array([12, 23, 26, 18])
 
-plt.plot(x, y, marker='.', markersize=10, markerfacecolor='#ad42f5', markeredgecolor='#f542b0')
+# plt.plot(x, y1, marker='.', markersize=10, markerfacecolor='#8a080f', markeredgecolor='#f542b0',
+#          linestyle='solid',
+#          linewidth = 1,
+#          color='#171617')
+
+# plt.plot(x, y2, marker='.', markersize=10, markerfacecolor='#8a080f', markeredgecolor='#f542b0',
+#          linestyle='solid',
+#          linewidth = 1,
+#          color='#171617')
+
+line_style = dict(marker='.', markersize=10, markerfacecolor='#8a080f', markeredgecolor='#f542b0',
+         linestyle='solid',
+         linewidth = 1,
+         color='#171617')
+
+plt.plot(x,y1, **line_style)
+plt.plot(x,y2, **line_style)
 plt.show()
