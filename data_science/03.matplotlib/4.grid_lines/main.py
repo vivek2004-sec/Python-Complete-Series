650ed6b4c0625ff7b3b80e87a8120f18798056e8
Vivek <vivekkamble762033@gmail.com>
2026-08-10 13:52:34 +0530

    added new things

---
+++ data_science/03.matplotlib/4.grid_lines/main.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# grid() = Helps make plots easier to read by adding reference lines.
+
+
+x = [1, 2, 3, 4, 5]
+y = [5, 10, 15, 10, 25]
+
+plt.grid(axis='both', linewidth=2)
+plt.plot(x,y, marker='.', 
+         markersize=10)
+plt.show()
