1ece8e79667a8c1a2e0cb996656280cdc7b9e1ad
Vivek <vivekkamble762033@gmail.com>
2026-08-10 19:51:44 +0530

    added new stuff

--- data_science/03.matplotlib/6.scatter_graphs/main.py
+++ data_science/03.matplotlib/6.scatter_graphs/main.py
@@ -4,10 +4,11 @@ import numpy as np
 # Scatter graph = Shows the relationship between two variables helps to identify a correlation (+, -, None)
 #                 Example = Study hours vs Test Scores
 
-x = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-y = [55, 70, 76, 79, 80, 84, 89, 94, 97]
+x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])
+y = np.array([55, 70, 76, 79, 80, 84, 89, 94, 97])
 
-plt.scatter(x,y, color='red')
+plt.scatter(x,y, color='skyblue', alpha=0.5,
+            s = 100)
 plt.title("Result Analysis", fontsize=15)
 plt.xlabel("Hours Studied", fontsize=15)
 plt.ylabel("Marks", fontsize=15)
