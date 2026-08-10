c636621909c960056316c0af251dbbb2c796e169
Vivek <vivekkamble762033@gmail.com>
2026-08-10 10:59:00 +0530

    added new stuff

--- data_science/03.matplotlib/1.intro/main.py
+++ data_science/03.matplotlib/1.intro/main.py
@@ -1,4 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
+# pyplot = gives user friendly interface for plotting.
+
+# x = np.array([2025, 2026, 2027, 2028])
+# y = np.array([10, 15, 20, 25])
+
+# plt.plot(x, y)
+# plt.show()
+
+std = np.array([[1, 2, 3], [4, 5, 6]])
+grade = np.array([[50, 45, 49], [39, 46, 45]])
+
+plt.plot(std,grade)
+plt.show()
---
+++ data_science/03.matplotlib/2.plot_customization/main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.array([2025, 2026, 2027, 2028])
+y = np.array([10, 15, 20, 15])
+
+plt.plot(x, y, marker='.', markersize=10, markerfacecolor='#ad42f5', markeredgecolor='#f542b0')
+plt.show()
