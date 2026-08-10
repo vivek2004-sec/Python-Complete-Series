a241b601e12c4c21e935ee2ed55912b134e399b0
Vivek <vivekkamble762033@gmail.com>
2026-08-11 00:42:47 +0530

    added new stuff

--- data_science/03.matplotlib/8.subplots/main.py
+++ data_science/03.matplotlib/8.subplots/main.py
@@ -7,7 +7,13 @@ import numpy as np
 
 x = np.array([1, 2, 3, 4, 5])
 
+y = np.array([7, 8, 9, 9, 4])
+
 figure, axes = plt.subplots(2, 2)
 axes[0, 0].plot(x, x*2)
 axes[0, 0].set_title("x*2", color='red')
+
+axes[0, 1].plot(x, y)
+axes[0, 1].set_title('x, y', color='red')
+
 plt.show()
