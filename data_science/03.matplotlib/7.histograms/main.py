be050101e3c17b2fdbfef162d6d97e94029c7f13
Vivek <vivekkamble762033@gmail.com>
2026-08-11 00:36:59 +0530

    added new stuff

--- data_science/03.matplotlib/7.histograms/main.py
+++ data_science/03.matplotlib/7.histograms/main.py
@@ -9,5 +9,9 @@ import numpy as np
         
 scores = np.random.normal(loc=80, scale=10, size=100)
 scores = np.clip(scores, 0, 100)
-plt.hist(scores, bins=20, color='cyan',edge='red')
+plt.hist(scores, bins=10, color='cyan', edgecolor='black')
+plt.grid(alpha=0.3)
+plt.title("Scores")
+plt.xlabel("Scores", color='red')
+plt.ylabel("# of students", color='red')
 plt.show()
---
+++ data_science/03.matplotlib/8.subplots/main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Figure = The entire canvas
+# Ax = A single plot (subplot)
+
+figure, axes = plt.subplots(2, 2)
+plt.show()
