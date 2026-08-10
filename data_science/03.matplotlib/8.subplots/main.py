87cb5b58010b0d39888aabd4be9ffaf774aa25d7
Vivek <vivekkamble762033@gmail.com>
2026-08-11 00:40:45 +0530

    added new stuff

--- data_science/03.matplotlib/8.subplots/main.py
+++ data_science/03.matplotlib/8.subplots/main.py
@@ -5,5 +5,9 @@ import numpy as np
 # Figure = The entire canvas
 # Ax = A single plot (subplot)
 
+x = np.array([1, 2, 3, 4, 5])
+
 figure, axes = plt.subplots(2, 2)
+axes[0, 0].plot(x, x*2)
+axes[0, 0].set_title("x*2", color='red')
 plt.show()
