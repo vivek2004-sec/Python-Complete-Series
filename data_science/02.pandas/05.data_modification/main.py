a131827599472008bd21c65f3569e615b9f8a31e
Vivek <vivekkamble762033@gmail.com>
2026-08-11 20:19:34 +0530

    addec new things

---
+++ data_science/02.pandas/05.data_modification/main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+data = {
+    'Name': ['vivek', 'sujal', 'sushant', 'sahil', 'samarth', 'sai', 'tanvi'], 
+    'Age' : [21, 20, 22, 22, 21, 21, 21 ],
+    'Scores': [97, 94, 90, 92, 99, 97, 100 ]
+}
+
+df = pd.DataFrame(data)
+
+
+# Adding columns 
+
+# 1. adding column via squarec bracket []
+
+df['gender'] = ['Male', 'Male', 'Male', 'Male','Male', 'Male', 'Female']
+
+print(df)
+
