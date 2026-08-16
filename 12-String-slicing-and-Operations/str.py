9f116e60bf7fc91c68ca7be5360cb375d09d514d
Vivek <vivekkamble762033@gmail.com>
2026-08-16 21:50:18 +0530

    added new stuff

--- 12-String-slicing-and-Operations/str.py
+++ 12-String-slicing-and-Operations/str.py
@@ -1,47 +1,51 @@
-name="Vivek,Harry,Chetan,Atharv,Sanket"
-v = "Chetan"
-print(v in name)  # Membership Operator
-print(len(name))
-print(name[0:18])
-print(name[0:5])
-print(name[0:11])
-print(name[6:11])
-print(name[0:5])
-print(name[0:-5])
-print(name[0:29])
+# name="Vivek,Harry,Chetan,Atharv,Sanket"
+# v = "Chetan"
+# print(v in name)  # Membership Operator
+# print(len(name))
+# print(name[0:18])
+# print(name[0:5])
+# print(name[0:11])
+# print(name[6:11])
+# print(name[0:5])
+# print(name[0:-5])
+# print(name[0:29])
 
 
 
-fruit= "Mango"
-len1= len(fruit)
-print("Mango is a",len1,"letter word.")
-print(fruit[0:4])
-print(fruit[1:4])
-print(fruit[:4])
-print(fruit[:])
-print(fruit[0:len(fruit)-3])
-print(fruit[-3:-1])
-print(fruit[-4:-2])
+# fruit= "Mango"
+# len1= len(fruit)
+# print("Mango is a",len1,"letter word.")
+# print(fruit[0:4])
+# print(fruit[1:4])
+# print(fruit[:4])
+# print(fruit[:])
+# print(fruit[0:len(fruit)-3])
+# print(fruit[-3:-1])
+# print(fruit[-4:-2])
 
-city= "Mumbai"
-len1=len(city)
-print("the length of city is", len1,"defined")
+# city= "Mumbai"
+# len1=len(city)
+# print("the length of city is", len1,"defined")
 
-print(city[0:1])
-print(city[0:2])
-print(city[0:3])
-print(city[0:4])
-print(city[0:5])
-print(city[0:6])
-print(city[:6])
-print(city[0:-3])
+# print(city[0:1])
+# print(city[0:2])
+# print(city[0:3])
+# print(city[0:4])
+# print(city[0:5])
+# print(city[0:6])
+# print(city[:6])
+# print(city[0:-3])
 
-vue= "Kamble"
-print(vue[0:-2])
-print(vue[0:-3])
-print(vue[0:-1])
+# vue= "Kamble"
+# print(vue[0:-2])
+# print(vue[0:-3])
+# print(vue[0:-1])
 
 
-nm = "Harry"
-print(nm[-4:-2])
-print(nm[-3:-1])
+# nm = "Harry"
+# print(nm[-4:-2])
+# print(nm[-3:-1])
+
+
+name = "vivek"
+print(name[0:-3])
