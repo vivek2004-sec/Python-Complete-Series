9216b8fbbddfa6bab6fc4f73c5c2bfff7c33e9c2
Vivek <vivekkamble762033@gmail.com>
2026-08-17 12:53:00 +0530

    undone the mistakes

--- data_science/03.matplotlib/2.plot_customization/main.py
+++ data_science/03.matplotlib/2.plot_customization/main.py
@@ -1,20 +1,28 @@
-def day_of_week(day):
-    match day:
-        case 1:
-            return "sunday"
-        case 2:
-            return "monday"
-        case 3:
-            return "tuesday"
-        case 4:
-            return "wednesday"
-        case 5:
-            return "thursday"
-        case 6:
-            return "friday"
-        case 7:
-            return "saturday"
-        case _:
-            return "not valid"
+import matplotlib.pyplot as plt
+import numpy as np
 
-print(day_of_week(3))
+
+x = np.array([2025, 2026, 2027, 2028])
+y1 = np.array([10, 15, 20, 15])
+y2 = np.array([12, 23, 26, 18])
+y3 = np.array([1, 13, 6, 28])
+
+# plt.plot(x, y1, marker='.', markersize=10, markerfacecolor='#8a080f', markeredgecolor='#f542b0',
+#          linestyle='solid',
+#          linewidth = 1,
+#          color='#171617')
+
+# plt.plot(x, y2, marker='.', markersize=10, markerfacecolor='#8a080f', markeredgecolor='#f542b0',
+#          linestyle='solid',
+#          linewidth = 1,
+#          color='#171617')
+
+line_style = dict(marker='.', markersize=10, markerfacecolor='#8a080f', markeredgecolor='#f542b0',
+         linestyle='solid',
+         linewidth = 1,
+         )
+
+plt.plot(x,y1,color='#69088a', **line_style)
+plt.plot(x,y2, color='#088a15', **line_style)
+plt.plot(x,y3, **line_style)
+plt.show()
