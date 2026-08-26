4e40607a638158884f3f312dfe3bc3b2fe4d0645
Vivek <vivekkamble762033@gmail.com>
2026-08-26 12:31:15 +0530

    added new stuff

--- 16-For_Loops/iterators/main.py
+++ 16-For_Loops/iterators/main.py
@@ -1,5 +1,26 @@
 # Iteration : It means taking each element of something one by one. any time you use the loop.
 
-num = [1, 2, 3]
-for i in num: 
-    print(i * 2 )
+# num = [1, 2, 3]
+# for i in num: 
+#     print(i * 2 )
+    
+# Iterator:  an iterator is an object that allows a programmer to traverse through a sequence of data without having to store the entire 
+# data in memory.
+import sys
+# x = [ l for l in range(1, 10000)]
+
+# for i in x:
+#     print(x * 2)
+
+
+
+# print(sys.getsizeof(x)/8)
+
+x = range(1, 10000)
+
+# for i in x:
+#     print(i * 2)
+    
+print(sys.getsizeof(x))
+
+# Iterable: object which one can iterate over.
