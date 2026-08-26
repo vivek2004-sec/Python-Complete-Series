b2109aae007857fa3dc1e335b8917e60c1730375
Vivek <vivekkamble762033@gmail.com>
2026-08-26 18:53:58 +0530

    added new stuff

--- 16-For_Loops/generators/main.py
+++ 16-For_Loops/generators/main.py
@@ -11,3 +11,19 @@ print(sys.getsizeof(L)) # gives size in bytes.
 
 x = range(10000000)
 print(sys.getsizeof(x))
+
+# example of Generator
+
+# generator is an actual function which has yield statement instead of return statement.
+
+def gen_rt():
+    yield "First statement"
+    yield "Second statement"
+    yield "Third statement"
+    
+    
+gen = gen_rt()
+print(gen)
+print(next(gen))
+print(next(gen))
+print(next(gen))
