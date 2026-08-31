7338999cdfe6b89b549247faa230155a5e2b554e
Vivek <vivekkamble762033@gmail.com>
2026-08-31 18:21:57 +0530

    added new stuff

---
+++ dsa/02.intro/main.py
@@ -0,0 +1,12 @@
+def inttostr(i):
+    digits = "0123456789"
+    if i == 0:
+        return '0'
+    result = " "
+    while i > 0:
+        result = digits[i%10] + result
+        i = i//10
+        
+    return result
+
+print(inttostr(123))
