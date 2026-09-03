b499843abfa8a16eea97b92743f4fa2d8c5f407e
Vivek <vivekkamble762033@gmail.com>
2026-09-03 20:46:25 +0530

    added new stuff

--- project/credit_card_validator/credit.py
+++ project/credit_card_validator/credit.py
@@ -12,3 +12,17 @@ credit_number = credit_number.replace("-", "")
 credit_number = credit_number.replace(" ", "")
 credit_number = credit_number[::-1]
 print(credit_number)
+
+# step 2
+
+for x in credit_number[::2]:
+    sum_odd_digits += int(x)
+    print(sum_odd_digits)
+    
+# step 3
+
+for x in credit_number[1::2]:
+    x = int(x) * 2
+    # if x > 10:
+        
+    
