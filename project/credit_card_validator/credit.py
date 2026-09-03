e0f6edf91e9446a5e701ade432f9fd090732f41f
Vivek <vivekkamble762033@gmail.com>
2026-09-03 20:50:10 +0530

    made final changes

--- project/credit_card_validator/credit.py
+++ project/credit_card_validator/credit.py
@@ -23,6 +23,19 @@ for x in credit_number[::2]:
 
 for x in credit_number[1::2]:
     x = int(x) * 2
-    # if x > 10:
+    if x >= 10:
+        sum_even_digits += (1 + (x % 10))
+    else:
+        sum_even_digits += x
         
+        
+# step 4
+total = sum_odd_digits + sum_even_digits
     
+    
+# step 5
+
+if total % 10 == 0:
+    print("Valid")
+else:
+    print("Invalid")
